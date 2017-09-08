0e20f58aee7983b0808ade6ca12af2b362ec177a
Rajeev Sharma <kr.rajeevsharma@gmail.com>
2017-09-08 15:02:41 +0530

    Worked on Twitter feed customization and meta data

--- js/app.js
+++ js/app.js
@@ -450,6 +450,33 @@ jQuery(document).ready(function($) {
   });
   // Performers carousel end
 
+  //Twitter widget
+  
+  var widgetWrapper=$(".twitter-widget-holder");
+	/* initialize scrollbar */
+	widgetWrapper.mCustomScrollbar({
+		theme:"dark-3",
+		scrollButtons:{enable:true}
+	})
+
+	/* cross-domain mousewheel hack */
+	.on("mouseenter",function(){
+		$(this).find("iframe").css("pointer-events","none");
+		}).on("mouseup",function(){
+			if(!$(this).find(".mCSB_scrollTools_onDrag").length) return;
+			setTimeout(function(){ widgetWrapper.trigger("mouseenter"); },1);
+		});
+	$(window).on("blur",function(){
+		widgetWrapper.find("iframe").css("pointer-events","auto");
+	}).on("focus",function(){
+		widgetWrapper.trigger("mouseenter");
+	
+	});
+
+      /* insert twitter widget js in window load fn */
+      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+	
+	//Twitter widget end here
 
 });
 
